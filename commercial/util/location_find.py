790894a0247aa6bcc6b3c96972d5ccbcbe047fd3
PanHu <zhanghu05@baidu.com>
2020-06-09 10:56:00 +0800

    给足迹添加位置

---
+++ commercial/util/location_find.py
@@ -0,0 +1,49 @@
+import requests
+
+from footprint.models import Footprint
+
+
+def location_to_address(location):
+    """
+    将经纬度转换为地址
+    所以选用的是逆地理编码的接口
+    :param location: 经纬度，格式为 经度+','+维度，例如:location='116.323294,39.893874'
+    :return:返回地址所在区，以及详细地址
+    """
+    parameters = {
+                    'location': location,
+                    'key': '0610416f549978a9ce5017dab6195a5a'
+                 }
+    base = 'http://restapi.amap.com/v3/geocode/regeo?'
+    response = requests.get(base, parameters)
+    answer = response.json()
+
+    return answer['regeocode']['addressComponent']['district'], answer['regeocode']['formatted_address']
+
+
+def filling_location_to_footprint():
+    """
+    给足迹填充 location
+    :return:
+    """
+    footprints = Footprint.objects.filter(id__gte=7519)
+
+    count = 0
+    print('has footprint: %s' % len(footprints))
+
+    for footprint in footprints:
+        try:
+            if footprint.location:
+                continue
+            lon_lat = '%s,%s' % (footprint.lon, footprint.lat)
+            location = location_to_address(lon_lat)
+            footprint.location = location
+            footprint.save()
+        except Exception as e:
+            print('filling location error: %s' % e)
+            continue
+
+        count += 1
+
+        if count % 100 == 0:
+            print('processed total footprint %s' % count)
